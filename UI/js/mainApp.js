d515276803abef9aab31d9b0701dae66d9fb2c51
joelatiam <joelatiam@googlemail.com>
2019-09-16 13:37:31 +0200

    chore(nav bar links): make it work [finishes #168508434]

--- UI/js/mainApp.js
+++ UI/js/mainApp.js
@@ -32,9 +32,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
     
     const categoryToDisplay = document.querySelector('.categories-list-selection');
-    if(categoryToDisplay){
-        topicsReady(categoryToDisplay);
-    }
+    // if(categoryToDisplay){
+    //     topicsReady(categoryToDisplay);
+    // }
 
     // home page
 
