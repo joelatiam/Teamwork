39bf7cc9bb8075def51306d9cb66e79ca7f2bf84
joelatiam <joelatiam@googlemail.com>
2019-09-24 14:23:53 +0200

    chome(code duplication): refactor [#168725073]

--- server/helpers/shortArticles.js
+++ server/helpers/shortArticles.js
@@ -1,12 +1,12 @@
 import myDB from '../models/myDB';
 
-const newtitle = (title) => {
-  let newTitle = title;
-  if (newTitle.length > 55) {
-    newTitle = title.slice(0, 52);
-    newTitle += '...';
+const shorten = (word, max) => {
+  let newWord = word;
+  if (newWord.length > max) {
+    newWord = word.slice(0, max - 3);
+    newWord += '...';
   }
-  return newTitle;
+  return newWord;
 };
 
 const dateFormat = (moment) => {
@@ -21,14 +21,7 @@ const dateFormat = (moment) => {
 
   return `${time} on ${date}`;
 };
-const shortArticle = (art) => {
-  let article = art;
-  if (article.length > 100) {
-    article = article.slice(0, 97);
-    article += '...';
-  }
-  return article;
-};
+
 const authorsName = (author) => {
   const user = myDB.users.find((u) => u.email === author);
   return `${user.firstName} ${user.lastName}`;
@@ -44,8 +37,8 @@ const shortPost = (post) => {
   } = post;
 
   const { id } = post;
-  article = shortArticle(article);
-  title = newtitle(title);
+  article = shorten(article, 100);
+  title = shorten(title, 55);
   date = dateFormat(date);
   topic = myDB.topics[post.topic];
   author = authorsName(author);
