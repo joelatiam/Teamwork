3f4e48b6af4b37d3bd575f54e1dde99045b126f0
joelatiam <joelatiam@googlemail.com>
2019-10-08 20:51:10 +0200

    chore (db config): heroku

--- v2/server/config/config.js
+++ v2/server/config/config.js
@@ -9,7 +9,14 @@ const config = {
   password: process.env.PGPASSWORD || null,
   port: process.env.PGPORT,
 };
-const pool = new pg.Pool(config);
+const pool = new pg.Pool(() => {
+  if (process.env.DATABASE_URL) {
+    return {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    };
+  } return config;
+});
 
 export default {
   secret,
